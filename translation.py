1de7a0b05497960bbf5c8aa07379919f192ef821
salomovasliddin3004 <85295223+salomovasliddin3004@users.noreply.github.com>
2021-09-19 14:23:17 +0500

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,12 +4,15 @@ class Translation(object):
     ABS_TEXT = " Iltimos, xudbinlik qilmang"
     UPGRADE_TEXT = "@All_Url_DownloaderBot <a href='https://t.me/SalomovAsliddin'> To'lovlar </a> \nTo'lovni to'lagandan so'ng, siz to'lovning skrinshotini @SalomovAsliddin_2oo4 yoki @SalomovAsliddinga yuborishingiz kerak."
     FORMAT_SELECTION = "Istagan formatingizni tanlang: <a href='{}'>fayl hajmi </a> \nAgar siz maxsus eskiz o'rnatmoqchi bo'lsangiz, quyidagi tugmalardan birini bosishdan oldin yoki keyin rasm yuboring."
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | filename | username | password"""
     NOYES_URL = "@All_Url_DownloaderBot URL manzili aniqlandi. Iltimos, xizmatni buzmang!"
     DOWNLOAD_START = "Server yuklanmoqda..."
     UPLOAD_START = "Telegram yuklanmoqda..."
     RCHD_BOT_API_LIMIT = "Ruxsat etilgan maksimal hajmdan kattaroq (50MB) Biroq, Biz uni o'rnatishga harakat qilyapmiz."
     RCHD_TG_API_LIMIT = "Kechirasiz. Ammo Telegram API cheklovlari tufayli 1,5 Gb dan katta fayllarni yuklay olmaymiz."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "Iltimos, menga foydali deb baho bering. https://t.me/SalomovAsliddin "
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @SalomovAsliddin \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Iltimos, /upgrade buyrug'i yordamida obunangizni yangilang."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Aniqlangan fayl hajmi: {}. Bepul foydalanuvchilar faqat yuklashi mumkin: {} \n Iltimos, /upgrade buyrug'i yordamida obunani yangilang. \n Botda xato topsangiz, bu erda men bilan bog'lanishingiz mumkin <ahref='https://uzfilms.rf.gd'> @SalomovAsliddin </a>"
     SAVED_CUSTOM_THUMB_NAIL = "Maxsus video/fayl eskizi saqlandi. Bu rasm video/faylda ishlatiladi."
@@ -20,7 +23,7 @@ class Translation(object):
     NO_CUSTOM_THUMB_NAIL_FOUND = "Maxsus eskiz topilmadi."
     NO_VOID_FORMAT_FOUND = "URLni tekshira olasizmi? URL formatidagi video formatini aniqlay olmayapman. Agar siz botda xato topsangiz, bu haqda xabar berishingiz mumkin t.me/SalomovAsliddin"
     USER_ADDED_TO_DB = "{} ga {} gacha Foydalanuvchi <a href='tg://user?id={|'> {} </a>  qo'shildi."
-    CURENT_PLAN_DETAILS = """ Mevcut plan detayları
+    CURENT_PLAN_DETAILS = """ Hozirgi reja tafsilotlari
 --------
 Telegram ID: <code>{}</code>
 Reja nomi: <a href='https://t.me/SalomovAsliddin'>{}</a>
@@ -35,6 +38,7 @@ Tugash sanasi: {}"""
 /me buyrug'i yordamida joriy reja tafsilotlarini ko'rish"""
     REPLY_TO_DOC_GET_LINK = "Yuqori tezlikda to'g'ridan -to'g'ri yuklab olish havolasini olish uchun Telegram vositasida javob bering"
     REPLY_TO_DOC_FOR_C2V = " Konvertatsiya qilish uchun Telegram vositasiga javob bering"
+    REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Maxsus eskiz yordamida nomini o'zgartirish uchun Telegram muhitiga javob bering (/rename)"
     AFTER_GET_DL_LINK = "<a href='{}'> yaratilgan </a> to'g'ridan to'g'ri yuklash havolasi {} kundan keyin tugaydi. \n © @All_Url_DownloaderBot"
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Sintaksis: /trim HH: MM: SS [HH: MM: SS]"""
@@ -48,3 +52,16 @@ Tugash sanasi: {}"""
     INVALID_UPLOAD_BOT_URL_FORMAT = "URL formati noto'g'ri. URL http: // yoki https: // bo'lishi kerak. Havolasini joylashtirishda ma'lum bir faylga nom berish uchun ushbu formatdan foydalaning <link> | fayl kengaytmasi"
     ABUSIVE_USERS = "Bu botdan foydalanishga ruxsat yo'q. Agar bu xato deb o'ylasangiz, bu erda <a href='https://UzFilms.rf.gd'> @SalomovAsliddin </a> bilan bog'laning va uni blokdan chiqaring."
     FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "@SalomovAsliddin "
+    
+    EXTRACT_ZIP_INTRO_ONE = "Zip file yuboring, zip filega replay tarda /unzip kamandasini yuboring"
+    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
+    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
+You can use /rename command after receiving file to rename it with custom thumbnail support."""
+    CANCEL_STR = "Process Cancelled"
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 30 minutes.
+/upgrade or Try 1800 seconds later."""
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
